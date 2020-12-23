42f3ecdceace978c9704c5eaa489378d44c9be38
black-sheep777 <forough.fallahfar@gmail.com>
2020-12-24 03:20:00 +0330

    changed eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,18 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/essential", "eslint:recommended","@vue/prettier"],
+  plugins:["prettier"],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "babel-eslint",
+    sourceType: 'module',
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
   rules: {
+    "prettier/prettier": "error",
+    "keyword-spacing": ["error", { before: true, after: true }],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+  }
 };
