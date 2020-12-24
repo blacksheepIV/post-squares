17144475108be4c562117c864d7905ee80206bcc
black-sheep777 <forough.fallahfar@gmail.com>
2020-12-24 11:44:20 +0330

    create new component for home page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h3>Hello from Home</h3>
+  </div>
+</template>
+
+<script>
+import { HTTP } from '@/utils/http-common.js'
+export default {
+  data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+
+  created() {
+    HTTP.get(`posts`)
+      .then(response => {
+        this.posts = response.data
+        console.log(this.posts)
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  height: 100vh;
+}
+</style>
