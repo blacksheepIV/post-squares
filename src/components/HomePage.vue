5d75d337ec93d5be1098ededf0eb02e34ff8319f
black-sheep777 <forough.fallahfar@gmail.com>
2020-12-25 17:52:01 +0330

    add post component

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="container">
-    <h3>Hello from Home</h3>
+  <div class="listing">
+    <h3>Post lists:</h3>
+    <div v-for="(row, index) in posts" :key="index" class="listing__row">
+      <div v-for="(item, i) in row" :key="i">
+        <Post :post-info="item" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { HTTP } from '@/utils/http-common.js'
+import Post from '@/components/Post.vue'
 export default {
+  components: {
+    Post
+  },
   data() {
     return {
       posts: [],
@@ -17,18 +26,31 @@ export default {
   created() {
     HTTP.get(`posts`)
       .then(response => {
-        this.posts = response.data
-        console.log(this.posts)
+        this.arrangeData(response.data)
       })
       .catch(e => {
         this.errors.push(e)
       })
+  },
+  methods: {
+    arrangeData(data) {
+      let rows = []
+      while (data.length > 0) {
+        rows.push(data.splice(0, 10))
+      }
+      console.log(rows)
+      this.posts = rows
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.container {
-  height: 100vh;
+.listing {
+  &__row {
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: 20px;
+  }
 }
 </style>
---
+++ src/components/Post.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card">
+    <h2>#{{ postInfo.id || 0 }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    postInfo: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  min-width: 50px;
+  width: 100%;
+  -webkit-box-shadow: 2px 5px 15px -8px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 2px 5px 15px -8px rgba(0, 0, 0, 0.75);
+  box-shadow: 2px 5px 15px -8px rgba(0, 0, 0, 0.75);
+  border-radius: 10px;
+  padding: 5px 5px;
+}
+</style>
